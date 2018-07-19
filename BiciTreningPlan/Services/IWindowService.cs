a3ae5ea7714d181780f07f977b2d28cd351e8287
Djordje Milenkovic <djordje.milenkovic96@hotmail.com>
2018-07-19 21:33:01 +0200

    add IWindowService, WindowService and RelayCommand to open new w

---
+++ BiciTreningPlan/Services/IWindowService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiciTreningPlan.Services
+{
+    public interface IWindowService
+    {
+       void showWindow(object dataContext, string title);
+    }
+}
---
+++ BiciTreningPlan/Services/WindowService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BiciTreningPlan.Services
+{
+    public class WindowService : IWindowService
+    {        
+        public void showWindow(object viewModel, string title)
+        {
+            var win = new Window();
+            win.Title=title;
+            win.Content = viewModel;
+            win.SizeToContent = SizeToContent.WidthAndHeight;
+            win.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            win.Show();
+        }
+    }
+}
