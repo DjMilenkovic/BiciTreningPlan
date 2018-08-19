7742628cb57bde7d9a6ed7ccb4f8e13cc9d86fbc
Djordje Milenkovic <djordje.milenkovic96@hotmail.com>
2018-08-19 17:18:16 +0200

    Change interface implementation in IskustveniNivoManipualtor

--- BiciTrainingPlanDAL/IskustveniNivoManipulator.cs
+++ BiciTrainingPlanDAL/IskustveniNivoManipulator.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace BiciTrainingPlanDAL
 {
-    class IskustveniNivoManipulator
+    class IskustveniNivoManipulator : IDataManipulator<Iskustveni_Nivo>
     {
+      
+        public void Create(ITable table)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Delete(long ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Iskustveni_Nivo> GetData()
+        {
+            using (var db = new ProjectDBEntities())
+            {
+                var query = from b in db.Iskustveni_Nivo
+                            orderby b.ID
+                            select b;
+
+                return query.ToList();
+            }
+        }
+        
+        public void Update(ITable table)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
